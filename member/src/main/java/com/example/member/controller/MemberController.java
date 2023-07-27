807a666f2275ace42d075b93f586b70318b75763
skangin <fluctuating5493@naver.com>
2023-07-27 18:21:54 +0900

    0727

---
+++ member/src/main/java/com/example/member/controller/MemberController.java
@@ -0,0 +1,28 @@
+package com.example.member.controller;
+
+import com.example.member.dto.MemberDTO;
+import com.example.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/member/save")
+    public String saveForm(){
+        return "save";
+    }
+    @PostMapping("/member/save")
+    public String save(@ModelAttribute MemberDTO memberDTO){
+        System.out.println("MemberController.save");
+        System.out.println("memberDTO = " + memberDTO);
+        memberService.save(memberDTO);
+        return "index";
+    }
+}
---
+++ myhome--4/src/main/java/com/godcoder/myhome/model/Board.java
@@ -0,0 +1,24 @@
+package com.godcoder.myhome.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    @Size(min=2, max=30, message = "제목은 2자이상 30자 이하입니다.")
+    private String title;
+    private String content;
+}
